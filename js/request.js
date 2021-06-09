0bd8d4b17cb18bfaed411c2d2c5977dea81e51c4
lmwis lamperouge <lmwis@lmwisdeMacBook-Pro.local>
2021-06-09 10:36:16 +0800

    feat:driver_receive page

--- js/request.js
+++ js/request.js
@@ -35,7 +35,7 @@ var Ajax = {
         // if (nickname.length == 0 || email.length == 0 || password.length == 0) {
         //     return;
         // }
-        var url = this.urlPre + "/driver/register";
+        var url = this.urlPre + "driver/register";
         var data = {
             name: name,
             sex: sex,
@@ -96,7 +96,7 @@ var Ajax = {
                         window.location.href='user_info.html';
                     }else{
                         $.cookie('driver_id',res.data.driverId);
-                        window.location.href='online_order.html';
+                        window.location.href='order_receive.html';
                     }
                 } else if (res.status === "fail") {
                     alert("登录失败：" + res.data.errMsg);
@@ -105,13 +105,15 @@ var Ajax = {
         });
     },
     publicOrder: function (item_volume, order_img_path, type, order_time, order_start, order_end, contact, contact_tel,
-                           product_desc, price) {
-        var url = this.urlPre + "/order/publishOrder";
+                           product_desc, price,order_time_line) {
+        var url = this.urlPre + "order/publishOrder";
         var data = {
+            item_count:1,
             item_volume: item_volume,
             order_img_path: order_img_path,
             truck_type: type,
             order_time: order_time,
+            order_time_line:order_time_line,
             order_start: order_start,
             order_end: order_end,
             contact: contact,
@@ -127,29 +129,33 @@ var Ajax = {
             xhrFields: {withCredentials: true},
             success: function (res) {
                 console.log(res);
-                if (res.status == "success") {
-                    alert("登录成功");
-                } else if (res.status == "fail") {
+                if (res.status === "success") {
+                    alert("提交成功");
+                } else if (res.status === "fail") {
                     alert("登录失败：" + res.data.errMsg);
                 }
             }
         });
     },
     getAllOrder: function () {
-        var url = this.urlPre + "/order/getAll";
+        var url = this.urlPre + "order/getAll";
+        let r ={};
         $.ajax({
             type: "GET",
             url: url,
             data: {},
             //处理跨域请求
             xhrFields: {withCredentials: true},
+            async:false,
             success: function (res) {
-                return res;
+                r = res;
             }
         });
+        return r;
     },
     getMyOrder: function (id) {
-        var url = this.urlPre + "/order/getMy";
+        var url = this.urlPre + "order/getMy";
+        let r = {};
         $.ajax({
             type: "GET",
             url: url,
@@ -157,19 +163,51 @@ var Ajax = {
                 driver_id: id
             },
             //处理跨域请求
+            async: false,
             xhrFields: {withCredentials: true},
             success: function (res) {
-                console.log(res);
-                if (res.status == "success") {
-                    alert("登录成功");
-                } else if (res.status == "fail") {
-                    alert("登录失败：" + res.data.errMsg);
-                }
+                r = res;
             }
         });
+        return r;
+    },
+    getReceive: function () {
+        var url = this.urlPre + "order/getReceive";
+        let r = {};
+        $.ajax({
+            type: "GET",
+            url: url,
+            data: {},
+            //处理跨域请求
+            async: false,
+            xhrFields: {withCredentials: true},
+            success: function (res) {
+                r = res;
+            }
+        });
+        return r;
+    },
+    receiveOrder:function (driver_id,order_id){
+        var url = this.urlPre + "driver/addOrder";
+        let r = {};
+        $.ajax({
+            type: "GET",
+            url: url,
+            data: {
+                driver_id,
+                order_id
+            },
+            //处理跨域请求
+            async: false,
+            xhrFields: {withCredentials: true},
+            success: function (res) {
+                r = res;
+            }
+        });
+        return r;
     },
     getOrderById: function (id) {
-        var url = this.urlPre + "/order/getById";
+        var url = this.urlPre + "order/getById";
         $.ajax({
             type: "GET",
             url: url,
